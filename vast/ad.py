729e3d30187dec3a5b90745c99d38b6c04b427a6
Dave Waldrum <dave.waldrum@juicemobile.com>
2016-05-19 20:16:15 +0000

    Making imports relative to module

--- vast/ad.py
+++ vast/ad.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from creative import Creative
+from .creative import Creative
 
 
 REQUIRED_INLINE = ['AdSystem', 'AdTitle']
